8261b86d94254b91cd31246e10f6c51aa57accc9
andreaburnes03 <a01285630@tec.mx>
2025-05-06 16:47:36 -0600

    chore(app): dashboard
    add layout and empty page

---
+++ src/app/[employeeID]/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import { DataTable, type SortDirection } from "@/components/ui/data-table"
+import { PageHeader } from "@/components/ui/header"
+import Image from "next/image"
+
+export default function dashboardPage() {
+
+}
