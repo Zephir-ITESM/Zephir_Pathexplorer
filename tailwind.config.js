29480e087ee3740c6b407350e5c265dbf69dc13b
Ricardo-HJ <a00837337@tec.mx>
2025-05-06 00:40:13 -0600

    refactor(root): add src folder
    change structure to src at root

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ import {heroui} from "@heroui/theme"
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
