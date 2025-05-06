5e7a9aeeba5b694fe509805a2975d907918caec2
Ricardo-HJ <a00837337@tec.mx>
2025-05-06 01:37:26 -0600

    style(app): metadata
    update metadata, app name and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,19 +4,17 @@ import clsx from "clsx";
 
 import { Metadata, Viewport } from "next";
 import { Providers } from "./providers";
-import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
+  title: "Zephir Pathexplorer",
+  description: "Human Resources Management System for Accenture",
   icons: {
-    icon: "/favicon.ico",
+    icon: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Accenture%20logo-AGEKhFu1kFFPMEmAqzDPslxaE0Dnpp.png",
+    apple: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Accenture%20logo-AGEKhFu1kFFPMEmAqzDPslxaE0Dnpp.png",
   },
-};
+}
+
 
 export const viewport: Viewport = {
   themeColor: [
