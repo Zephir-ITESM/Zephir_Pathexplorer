7a14097cf5b632ede298f5dc13f080123b9ffcee
Ricardo-HJ <a00837337@tec.mx>
2025-05-06 01:19:23 -0600

    build(root): add dependency and allow blob
    add class-variance-authority and allows usage of remote images stored in vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'v0.blob.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+          port: '',
+          pathname: '/**',
+        }
+      ],
+    },
+  }
+  
+  module.exports = nextConfig
