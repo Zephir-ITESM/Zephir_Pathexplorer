70309c4e1153349947825fc62befcba072854a4a
andreaburnes03 <a01285630@tec.mx>
2025-05-09 14:43:03 -0600

    refctor(login): components
    use heorui components instead of custom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light", forcedTheme: "light" }}>
           {children}
         </Providers>
       </body>
